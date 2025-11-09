97c6bdd167f0061900d14615b812586cc03a82cc
qomar-lail <qomaruddinlail@gmail.com>
2025-11-09 21:16:36 +0700

    Update tambah_detail_transaksi.php

--- Modul-6/24-128_qomaruddin/data_base/tambah_data/tambah_detail_transaksi.php
+++ Modul-6/24-128_qomaruddin/data_base/tambah_data/tambah_detail_transaksi.php
@@ -1,1 +1,15 @@
 
+<?php
+require_once __DIR__. "../../koneksi.php";
+
+$select_harga = "SELECT harga FROM barang WHERE nama_barang = '$barang'";
+$data = $conn->query($select_harga);
+$row = $data->fetch_assoc();
+$harga = $row["harga"]*$quantity;
+
+$query_insert = "INSERT INTO transaksi_detail(transaksi_id,barang_id,harga,qty) VALUES('$id_transaksi','$id',$harga,'$quantity')";
+if($conn->query($query_insert)){
+    header("location:.././index.php");
+}
+
+?>
