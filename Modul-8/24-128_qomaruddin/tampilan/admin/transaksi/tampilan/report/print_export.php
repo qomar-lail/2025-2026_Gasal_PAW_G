fbdc2b4fe8f966a8d3477331674b9ece628e4558
qomar-lail <qomaruddinlail@gmail.com>
2025-11-24 19:58:55 +0700

    Create print_export.php

---
+++ Modul-8/24-128_qomaruddin/tampilan/admin/transaksi/tampilan/report/print_export.php
@@ -0,0 +1,4 @@
+<div class="mt-3 pe-2 d-flex justify-content-start no-print " id="<?=$tipe?>">
+    <button onclick="window.print()" class="btn btn-warning me-1">Cetak</button>
+    <a href="../../tampilan/admin/transaksi/tampilan/cetak/export_to_excel.php?awal=<?= $tanggal_awal ?>&akhir=<?= $tanggal_akhir ?>" class="btn btn-warning">Excel</a>
+</div>
