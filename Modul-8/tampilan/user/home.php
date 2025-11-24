5fb8df45c47155cc1fc0cff8db0106249aa55628
qomar-lail <qomaruddinlail@gmail.com>
2025-11-24 11:08:43 +0700

    Create home.php

---
+++ Modul-8/tampilan/user/home.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if(!isset($_SESSION["login"])|| $_SESSION["level"] != 2){
+    header("location:../form_login.php");
+    echo"masuk";
+}
+$page = "home";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Form-Login</title>
+</head>
+<body>
+    <div class="container w-100 d-flex justify-content-center flex-column align-items-center">
+        <?php require_once __DIR__. "/navbar.php" ?>
+    </div>
+</body>
+</html>
